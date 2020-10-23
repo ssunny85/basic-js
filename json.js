e5e459be2d17f0c1059c8cc4b4c9721243461710
ssunny <ssssunny85@gmail.com>
2020-10-23 10:35:30 +0900

    Add json.js

---
+++ json.js
@@ -0,0 +1,40 @@
+// JSON
+// Javascript Object Notation
+
+// 1. Object to JSON
+// stringify(obj)
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(['apple', 'banana']);
+console.log(json);
+
+const rabbit = {
+    name: 'tori',
+    color: 'white',
+    size: null,
+    birthDate: new Date(),
+    jump: () => {
+        console.log(`${name} can jump!`);
+    },
+};
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ['name', 'color']);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+    console.log(`key: ${key}, value: ${value}`);
+    return key === 'name' ? 'ellie' : value;
+});
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
+console.clear();
+json = JSON.stringify(rabbit);
+const obj = JSON.parse(json);
+console.log(obj);
+rabbit.jump();
+// obj.jump();
